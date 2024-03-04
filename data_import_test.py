a1dd8873d5b1d2e3c8f30da7569700842b908c45
sanketparm <sanketparm@gmail.com>
2024-03-04 11:55:03 +0000

    test spark full Load

--- data_import_test.py
+++ data_import_test.py
@@ -8,42 +8,34 @@ spark = SparkSession.builder \
     .appName("HealthInsurance") \
     .enableHiveSupport() \
     .getOrCreate()
-#.config("spark.jars", "/Users/hmakhlouf/Desktop/TechCnsltng_WorkSpace/config/postgresql-42.7.2.jar") \
 
-# something else
 
-# PostgreSQL connection properties
 postgres_url = "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb"
 postgres_properties = {
     "user": "consultants",
     "password": "WelcomeItc@2022",
     "driver": "org.postgresql.Driver",
 }
-postgres_table_name = "health-insurance1"
-
+postgres_table_name = "health_insurance"
 
 # Read data from PostgreSQL
 df_postgres = spark.read.jdbc(url=postgres_url, table=postgres_table_name, properties=postgres_properties)
 df_postgres.show()
 
 
-
 # full load data to hive
 
 # Create database
-#spark.sql("CREATE DATABASE IF NOT EXISTS project1db")
+spark.sql("CREATE DATABASE IF NOT EXISTS sanket_db")
 
 # Hive database and table names
-#hive_database_name = "project1db"
-#hive_table_name = "carInsuranceClaims"
+hive_database_name = "sanket_db"
+hive_table_name = "health_insurance"
 
 # Create Hive Internal table
 df_postgres.write.mode('overwrite') \
-    .saveAsTable("sanket_db.healthInsurance")
+    .saveAsTable("sanket_db.health_insurance")
 
 # Read Hive table
-df = spark.read.table("sanket_db.healthInsurance")
+df = spark.read.table("sanket_db.health_insurance")
 df.show()
-
-# Stop Spark session
-spark.stop()
